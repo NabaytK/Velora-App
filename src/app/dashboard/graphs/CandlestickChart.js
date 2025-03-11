2c1f87bdd410ec276b918a0d522ba4643f79e345
NabaytK <nkibreab@student.ysu.edu>
2025-03-11 17:23:48 -0400

    Create CandlestickChart.js

---
+++ src/app/dashboard/graphs/CandlestickChart.js
@@ -0,0 +1,6 @@
+"use client";  // Ensure it runs as a client component
+import React from "react";
+
+export default function CandlestickChart() {
+  return <div>Candlestick Chart Placeholder</div>;
+}
